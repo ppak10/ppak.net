c5a0470301c318e2bf395b09223c0f509a4e0968
Peter Pak <ppak10@gmail.com>
2026-04-05 13:23:10 -0400

    Updates to always use person profiles

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -11,6 +11,7 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN as string, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
       defaults: '2026-01-30',
+      person_profiles: 'always',
     });
   }, []);
 
